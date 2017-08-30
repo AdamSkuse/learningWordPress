a6878f8b316baf61ee52515433dbb43142e44f53
AdamSkuse <a.t.skuse@gmail.com>
2017-08-30 14:20:31 -0400

    Refactors code into content.php

--- archive.php
+++ archive.php
@@ -31,39 +31,9 @@
 </h2>
 
 <?php
-  while (have_posts()) : the_post(); ?>
-
-  <article class="post">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-   		<p class="post-info"><?php the_time('F j, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
-      echo 'Day archive: ' . get_the_date();
-
-
-			<?php
-			
-			$categories = get_the_category();
-			$separator = ", ";
-			$output = '';
-			
-			if ($categories) {
-				
-				foreach ($categories as $category) {
-				
-					$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;
-					
-				}
-				
-				echo trim($output, $separator);
-				
-			}
-			
-			?>
-
-  </p>
-    <?php the_excerpt(); ?>
-  </article>
-
-  <?php endwhile;
+  while (have_posts()) : the_post();
+    get_template_part('content');
+  endwhile;
 
   else :
     echo '<p>No content found!</p>';
